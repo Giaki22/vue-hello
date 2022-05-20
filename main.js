fb78c7621d3bc5a65bb962d8a39de29f5f0f0067
Giaki22 <giaki22@hotmail.com>
2022-05-20 09:20:27 +0200

    + Image

--- main.js
+++ main.js
@@ -2,5 +2,6 @@ let app = new Vue({
     el: '#app',
     data:{
         title: "Hello Vue!",
+        img: 'https://media.istockphoto.com/photos/explosion-of-colored-powder-on-black-background-picture-id531706796?k=20&m=531706796&s=612x612&w=0&h=pHNUmQXnFqghhZvXcMWJxZJVaE-Cyek8vkX-t1U98_o='
     }
 })
